7415aed93d2609b87d9496cdf1566da5e6483dd8
Denise Ho <deniseho@users.noreply.github.com>
2016-03-10 07:21:09 +0800

    Added files via upload

---
+++ homework1/homework1Tests/FinancialReport.cs
@@ -0,0 +1,10 @@
+﻿namespace homework1
+{
+    public class FinancialReport
+    {
+        public int Id { get; set; }
+        public int Cost { get; set; }
+        public int Revenue { get; set; }
+        public int SellPrice { get; set; }
+    }
+}
